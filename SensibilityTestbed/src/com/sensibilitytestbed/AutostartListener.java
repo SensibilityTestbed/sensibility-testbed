78a85a7cff575c2bd701092bf7de562194834f7a
yyzhuang <yanyanzhuang83@gmail.com>
2014-07-27 18:26:00 -0700

    fix auto start

--- SensibilityTestbed/src/com/sensibilitytestbed/AutostartListener.java
+++ SensibilityTestbed/src/com/sensibilitytestbed/AutostartListener.java
@@ -13,10 +13,13 @@ import android.util.Log;
  * Listens for a BOOT_COMPLETED intent and starts the application if AUTOSTART_ON_BOOT is set
  *
  */
-public class AutostartListener extends BroadcastReceiver {
+public class AutostartListener extends BroadcastReceiver {	
 	@Override
 	public void onReceive(final Context context, Intent intent) {
 		// Executed on successful booting
+		Intent i = new Intent(context, ScriptActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(i);
 		new AsyncStart(context).execute();
 	}
 	public class AsyncStart extends AsyncTask<Void, Integer, String> {
@@ -26,12 +29,12 @@ public class AutostartListener extends BroadcastReceiver {
 	        this.context = context;
 	    }
 	    @Override
-	    protected String doInBackground(Void... params) {
+	    protected String doInBackground(Void... params) {		
 	    	while (true) {		
-	    		boolean isInstalled = (new File(ScriptActivity.getSeattlePath()+"seattle_repy/","nmmain.py")).exists();
+	    		boolean isInstalled = ScriptActivity.isSeattleInstalled;	    		
 	    		SharedPreferences settings = context.getSharedPreferences(ScriptActivity.SEATTLE_PREFERENCES, Context.MODE_WORLD_WRITEABLE);
 	    		// Check if the app is installed and is to be run on startup
-	    		if(isInstalled&&settings.getBoolean(ScriptActivity.AUTOSTART_ON_BOOT,true)) {
+	    		if(isInstalled && settings.getBoolean(ScriptActivity.AUTOSTART_ON_BOOT,true)) {
 			    	  Intent serviceIntent = new Intent();
 			    	  serviceIntent.setAction("com.sensibilitytestbed.ScriptService");
 			    	  ScriptService.serviceInitiatedByUser = true;
